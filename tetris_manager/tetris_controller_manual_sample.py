b6e2dc337e56b0eb9b76129c6de4396b951d9a77
seigot <s.takada.3o3@gmail.com>
2020-10-18 16:20:18 +0900

    add tetris_controller_manual_sample.py

---
+++ tetris_manager/tetris_controller_manual_sample.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+class TetrisController(object):
+
+    def GetNextMove(self, TetrisStatus):
+        # if play manually, return None
+        return None
+
+TETRIS_CONTROLLER = TetrisController()
+
