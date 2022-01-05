95c04d305d2eeae2b32fc2d07beccf2132f45aa2
seigot <s.takada.3o3@gmail.com>
2022-01-05 15:24:15 +0000

    add file for debug

---
+++ doc/old/stop.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+function try_kill_process(){
+    PROCESS_NAME=$1
+    if [ -z "$PROCESS_NAME" ]; then
+	return 0
+    fi
+    
+    PROCESS_ID=`ps -e -o pid,cmd | grep ${PROCESS_NAME} | grep -v grep | awk '{print $1}'`
+    if [ -z "$PROCESS_ID" ]; then
+	echo "no process like... ${PROCESS_NAME}"
+	return 0
+    fi
+    echo "kill process ... ${PROCESS_NAME}"
+    #kill -SIGINT $PROCESS_ID
+    kill $PROCESS_ID
+}
+
+function stop_process(){    
+    try_kill_process "start.py"
+    try_kill_process "game_manager.py"
+}
+
+stop_process
