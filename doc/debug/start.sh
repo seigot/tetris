65d5f5b9789445bade4b9495a741e564693488eb
seigot <s.takada.3o3@gmail.com>
2022-01-07 23:44:57 +0900

    update debug scripts

--- doc/debug/start.sh
+++ doc/debug/start.sh
@@ -2,8 +2,7 @@
 
 ## default value
 VALUE_L="1"
-IS_MANUAL_CONTROLL="n"
-IS_SAMPLE_CONTROLL="n"
+IS_MODE="n"
 GAME_TIME="180"                  # game time (s)
 RESULT_LOG_JSON="result.json"    # result log file
 INPUT_RAMDOM_SEED=""             # random seed
@@ -13,15 +12,14 @@ while getopts l:m:s:t:f:r: OPT
 do
   case $OPT in
     "l" ) VALUE_L="$OPTARG" ;;
-    "m" ) IS_MANUAL_CONTROLL="$OPTARG" ;;
-    "s" ) IS_SAMPLE_CONTROLL="$OPTARG" ;;
+    "m" ) IS_MODE="$OPTARG" ;;
     "t" ) GAME_TIME="$OPTARG" ;;
     "f" ) RESULT_LOG_JSON="$OPTARG" ;;
     "r" ) INPUT_RAMDOM_SEED="$OPTARG" ;;
   esac
 done
 echo "level: $VALUE_L"
-echo "is_manual_controll: $IS_MANUAL_CONTROLL"
+echo "is_manual_controll: $IS_MODE"
 
 ## set field parameter for level 1
 DROP_SPEED="1000"        # drop speed (s)
@@ -47,4 +45,4 @@ echo "OBSTACLE_HEIGHT: ${OBSTACLE_HEIGHT}"
 echo "OBSTACLE_PROBABILITY: ${OBSTACLE_PROBABILITY}"
 
 ## start game
-python3 game_manager/game_manager.py --game_time ${GAME_TIME} --seed ${RANDOM_SEED} --obstacle_height ${OBSTACLE_HEIGHT} --obstacle_probability ${OBSTACLE_PROBABILITY} --drop_speed ${DROP_SPEED} --manual ${IS_MANUAL_CONTROLL} --use_sample ${IS_SAMPLE_CONTROLL} --resultlogjson ${RESULT_LOG_JSON}
+python3 game_manager/game_manager.py --game_time ${GAME_TIME} --seed ${RANDOM_SEED} --obstacle_height ${OBSTACLE_HEIGHT} --obstacle_probability ${OBSTACLE_PROBABILITY} --drop_interval ${DROP_SPEED} --mode ${IS_MODE} --resultlogjson ${RESULT_LOG_JSON}
