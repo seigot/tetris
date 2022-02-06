fa8f14eec8985bd5718a5868a319689b4356372e
bushio <kuromady@gmail.com>
2022-02-06 15:28:36 +0900

    add model modify move_next

---
+++ game_manager/model/deepqnet.py
@@ -0,0 +1,24 @@
+import torch.nn as nn
+
+class DeepQNetwork(nn.Module):
+    def __init__(self,input_dim):
+        super(DeepQNetwork, self).__init__()
+
+        self.conv1 = nn.Sequential(nn.Linear(input_dim, 64), nn.ReLU(inplace=True))
+        self.conv2 = nn.Sequential(nn.Linear(64, 64), nn.ReLU(inplace=True))
+        self.conv3 = nn.Sequential(nn.Linear(64, 1))
+        
+        self._create_weights()
+
+    def _create_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Linear):
+                nn.init.xavier_uniform_(m.weight)
+                nn.init.constant_(m.bias, 0)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = self.conv3(x)
+
+        return x
