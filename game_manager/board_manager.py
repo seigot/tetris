894a1af64a0792b5e1f048d605967e346f765955
seigot <s.takada.3o3@gmail.com>
2022-12-29 03:39:56 +0900

    update to implement hold function https://github.com/seigot/tetris/issues/13

--- game_manager/board_manager.py
+++ game_manager/board_manager.py
@@ -112,6 +112,7 @@ class BoardData(object):
         self.currentDirection = 0
         self.currentShape = Shape() # initial current shape data
         self.nextShape = None
+        self.holdShape = None
         self.shape_info_stat = [0] * 8
         self.obstacle_height = 0
         self.obstacle_probability = 0
@@ -160,11 +161,13 @@ class BoardData(object):
         length = len(self.ShapeList)
         return length
 
-    def getShapeData(self, ShapeNumber):
+    def getShapeDataFromShapeClass(self, ShapeClass):
 
-        ShapeClass = self.ShapeList[ShapeNumber]
+        if ShapeClass == None:
+            return None, None, None
+
+        #ShapeClass = self.ShapeList[ShapeNumber]
         ShapeIdx = ShapeClass.shape
-
         ShapeRange = (0, 1, 2, 3)
         if ShapeIdx in (Shape.shapeI, Shape.shapeZ, Shape.shapeS):
             ShapeRange = (0, 1)
@@ -174,6 +177,13 @@ class BoardData(object):
             ShapeRange = (0, 1, 2, 3)
 
         return ShapeClass, ShapeIdx, ShapeRange
+
+    def getShapeData(self, ShapeNumber):
+        ShapeClass = self.ShapeList[ShapeNumber]
+        return self.getShapeDataFromShapeClass(ShapeClass)
+
+    def getholdShapeData(self):
+        return self.getShapeDataFromShapeClass(self.holdShape)
 
     def getCurrentShapeCoord(self):
         return self.currentShape.getCoords(self.currentDirection, self.currentX, self.currentY)
@@ -305,6 +315,24 @@ class BoardData(object):
             #print("failed to rotateLeft..")
             return False
         return True
+
+    def setholdShape(self):
+        if self.holdShape == None:
+            # if holdShape not exists, set holdShape
+            self.holdShape = self.currentShape
+            self.createNewPiece()
+            return 1
+        else:
+            # if holdShape exists, exchange shapes
+            tmp = self.holdShape
+            self.holdShape = self.currentShape
+            self.currentShape = tmp
+            # init current X,Y,Direction
+            minX, maxX, minY, maxY = self.nextShape.getBoundingOffsets(0)
+            self.currentX = 5
+            self.currentY = -minY
+            self.currentDirection = 0
+        return 0
 
     def removeFullLines(self):
         newBackBoard = [0] * BoardData.width * BoardData.height
