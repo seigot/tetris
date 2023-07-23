6b40bfcf57bd8e6c73763b1b812ecd6e33627040
seigot <s.takada.3o3@gmail.com>
2023-07-23 14:12:30 +0900

    if art config format is invalid, return error.

--- game_manager/board_manager.py
+++ game_manager/board_manager.py
@@ -202,8 +202,8 @@ class BoardData(object):
             self.nextShapeIndexList = [ block_order[ii][0] for ii in range(len(block_order))]
             self.nextShapeIndexListDXY = [[block_order[ii][1],block_order[ii][2],block_order[ii][3]] for ii in range(len(block_order))]
         except Exception as e:
-            #print(e)
-            pass
+            print(e)
+            exit()
 
     #######################################
     ## 画面ボードデータを返す
